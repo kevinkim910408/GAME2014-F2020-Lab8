4948b0bf32392b726f1b94a549a405f04a838b0a
kevinkim910408 <kevinkim0408@naver.com>
2020-11-18 03:55:41 +0900

    Initial commit

---
+++ Assets/_Scripts/BulletController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour, IApplyDamage
+{
+    public float verticalSpeed;
+    public float verticalBoundary;
+    public int damage;
+    public Vector3 direction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+
+    private void _Move()
+    {
+        transform.position += direction * verticalSpeed * Time.deltaTime;
+    }
+
+    private void _CheckBounds()
+    {
+        if (transform.position.y > verticalBoundary)
+        {
+            BulletManager.Instance().ReturnBullet(gameObject);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        //Debug.Log(other.gameObject.name);
+        BulletManager.Instance().ReturnBullet(gameObject);
+    }
+
+    public int ApplyDamage()
+    {
+        return damage;
+    }
+}
---
+++ Assets/_Scripts/LOS.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LOS : MonoBehaviour
+{
+    public Collider2D collidesWith;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collidesWith = collision;
+    }
+}
